b3edcda0c7d17dd67d3fdad7f64d40ea4f2308eb
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-29 16:29:00 +0200

    Add btn riprova

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const lista_utenti = ['federico@gmail.com', 'alessio@gmail.com', 'fernanda@gmail
 
 const login = document.querySelector('.email');
 const accedi_btn = document.querySelector('.accedi');
+const riprova_btn = document.querySelector('.riprova');
 
 accedi_btn.addEventListener('click', function(){
 
@@ -28,6 +29,31 @@ const accesso = document.querySelector('.accesso');
     } else {
         //console.log('non sei registratro');
         accesso.innerHTML += 'Non sei registrato';
+    }
+});
+
+riprova_btn.addEventListener('click', function () {
+
+    let registrato;
+
+    for (let i = 0; i < lista_utenti.length; i++) {
+        const element = lista_utenti[i];
+
+        //controlla che sia nella lista di chi può accedere
+        if (login.value == element) {
+            registrato = true;
+        }
+    }
+
+    const accesso = document.querySelector('.accesso');
+
+    //stampa un messaggio appropriato sull’esito del controllo.
+    if (registrato == true) {
+        //console.log('Sei registrato');
+        accesso.innerHTML = '';
+    } else {
+        //console.log('non sei registratro');
+        accesso.innerHTML = '';
     }
 });
 
