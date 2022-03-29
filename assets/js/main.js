3d605573050d53ccc426bc46619cc1db068d9710
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-29 15:06:45 +0200

    Add js email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,45 @@
-/* 
-Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-non bisogna ricordarsi a memoria come generare dei numeri random, c'é W3S per questo
-*/
+
+//Chiedi all’utente la sua email
+const lista_utenti = ['federico@gmail.com', 'alessio@gmail.com', 'fernanda@gmail.com', 'gabriella@gmail.com', 'alberto@hotmail.com', 'vincenzo@libero.it'];
+
+const login = document.querySelector('.email');
+const accedi_btn = document.querySelector('.accedi');
+
+accedi_btn.addEventListener('click', function(){
+
+
+    for (let i = 0; i < lista_utenti.length; i++) {
+        const element = lista_utenti[i];
+
+        //controlla che sia nella lista di chi può accedere
+        if (login.value == element) {
+            //stampa un messaggio appropriato sull’esito del controllo.
+            console.log('sei registrato');
+        } else if (login.value != element) {
+            //stampa un messaggio appropriato sull’esito del controllo.
+            console.log('non sei registrato');
+        }
+    }
+});
+
+
+
+//gioco dei dadi
+
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+/* let random_number_gamer = Math.ceil(Math.random()* 6);
+console.log(random_number_gamer);
+let random_number_computer = Math.ceil(Math.random() * 6);
+console.log(random_number_computer);
+
+//Stabilire il vincitore, in base a chi fa il punteggio più alto
+if (random_number_gamer > random_number_computer) {
+    console.log('hai vinto');
+} else if (random_number_gamer == random_number_computer){
+    console.log('pareggio');
+} else {
+    console.log('ha vinto il computer');
+}
+
+
+ */
